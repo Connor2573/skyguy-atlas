2876410db10332c0ce6a21078291bc1458d0262d
Connor2573 <Connor.park2573@gmail.com>
2023-05-11 16:29:58 -0400

    pushing up cuda changes

--- models/auto_encoder/model.py
+++ models/auto_encoder/model.py
@@ -6,8 +6,8 @@ class Encoder(torch.nn.Module):
         #encoder layers
         self.conv1 = torch.nn.Conv2d(in_channels=in_channels, out_channels=256, kernel_size=3, stride=1, padding=1)
         self.conv2 = torch.nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.conv3 = torch.nn.Conv2d(in_channels=128, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.conv4 = torch.nn.Conv2d(in_channels=32, out_channels=8, kernel_size=3, stride=1, padding=1)
+        self.conv3 = torch.nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv4 = torch.nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.flatten = torch.nn.Flatten(start_dim=0)
         self.linear_out = torch.nn.Linear(flattened_dim, 2573) #2573 is made up
         self.activation = torch.nn.LeakyReLU()
@@ -35,8 +35,8 @@ class Decoder(torch.nn.Module):
         #decoder layers
         self.rebuild_shape = rebuild_shape
         self.linear_in = torch.nn.Linear(2573, flattened_dim)
-        self.conv4 = torch.nn.ConvTranspose2d(in_channels=8, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.conv3 = torch.nn.ConvTranspose2d(in_channels=32, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv4 = torch.nn.ConvTranspose2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = torch.nn.ConvTranspose2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.conv2 = torch.nn.ConvTranspose2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
         self.conv1 = torch.nn.ConvTranspose2d(in_channels=256, out_channels=3, kernel_size=3, stride=1, padding=1)
         self.activation = torch.nn.LeakyReLU()
@@ -63,7 +63,7 @@ class AE(torch.nn.Module):
     def __init__(self, input_dims):
         super(AE, self).__init__()
         in_channels = input_dims[0]
-        last_conv_layer_size = 8
+        last_conv_layer_size = 32
         flattened_dim = last_conv_layer_size * input_dims[1] * input_dims[2] #64 is the out channels of the last convulutional layer of the encoder
         shape = (last_conv_layer_size, input_dims[1], input_dims[2])
 
--- models/auto_encoder/train.py
+++ models/auto_encoder/train.py
@@ -1,10 +1,12 @@
 import torch, torchvision
-import model
+import model as my_models
 from torchvision import datasets, models, transforms
 
-epochs = 40
-x_size = 130
-y_size = 100
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+epochs = 50
+x_size = 150
+y_size = 120
 
 def load_training(root_path, dir, batch_size, **kwargs):
 
@@ -13,13 +15,14 @@ def load_training(root_path, dir, batch_size, **kwargs):
          transforms.ToTensor()])
     data = datasets.ImageFolder(root=root_path + dir, transform=transform)
     train_loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=True, **kwargs)
-
     return train_loader
 
-#model = model.AE((3, x_size, y_size))
-model = torch.load('./models/auto_encoder/mk1.pth')
+model = my_models.AE((3, x_size, y_size))
+#model = torch.load('./models/auto_encoder/mk1.pth')
 
-loader = load_training('./datasets/', 'raw_images', 32)
+model.to(device)
+
+loader = load_training('./datasets/', 'raw_images', 1)
 optimizer = torch.optim.Adam(model.parameters(), lr=.001)
 loss_fn = torch.nn.MSELoss()
 
@@ -27,7 +30,7 @@ model.train()
 for epoch in range(epochs):
     for batch in loader:
         for image in batch[0]:
-
+            image = image.to(device)
             prediction= model(image)
 
             RMSE_loss = torch.sqrt(loss_fn(prediction, image))
